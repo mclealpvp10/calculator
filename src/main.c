3cc9ffe22e4fda89569b60f50086bf5821fd4ec0
Matheus <mclealpvp10@gmail.com>
2025-12-24 12:43:06 -0300

    testar função de divisão

--- src/main.c
+++ src/main.c
@@ -14,7 +14,7 @@ int main(){
   printf("Digite o 2º número: ");
   scanf("%f", &num2);
   printf("OK!\n");
-  printf("Digite a operação: (+, -)\n");
+  printf("Digite a operação: (+, -, /)\n");
   scanf(" %c", &operacao);
   if (operacao ==  '+') {
     float resultado = num1 + num2;
@@ -22,7 +22,11 @@ int main(){
   } 
   else if (operacao == '-') {
     float resultado = num1 - num2; 
-    printf("O resultado da conta %.2f - %.2f é %.2f\n", num1, num2, resultado);
+    printf("O resultado da conta %.2f + %.2f é %.2f\n", num1, num2, resultado);
+  } 
+  else if (operacao == '/') {
+    float resultado = num1 / num2;
+    printf("O resultado da conta %.2f / %.2f é %.2f\n", num1, num2, resultado);
   } else {
     printf("Inválido!\n");
   }
