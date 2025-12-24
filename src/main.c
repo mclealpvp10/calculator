713fe1d805e5665c898d5c33d828bfe689288f11
Matheus <mclealpvp10@gmail.com>
2025-12-24 12:29:19 -0300

    add: escanear entrada de operação (+ ou -) do usuário

--- src/main.c
+++ src/main.c
@@ -6,12 +6,22 @@
 int main(){
   float num1;
   float num2;
+  char operacao;
+
   printf("Digite o 1º número: ");
   scanf("%f", &num1);
   printf("Digite o 2º número: ");
   scanf("%f", &num2);
   printf("OK!\n");
-  printf("1: %f, 2: %f\n", num1, num2);
-  return 0;
-
+  printf("Digite a operação: (+, -)\n");
+  scanf(" %c", &operacao);
+  if (operacao ==  '+') {
+    printf("Mais\n");
+  } 
+  else if (operacao == '-') {
+    printf("Menos\n");
+  } else {
+    printf("Inválido!\n");
+  }
+  return 0;  
 }
