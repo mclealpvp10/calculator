99b9d1cc0881528b0fc3607b5485a665c1296512
Matheus <mclealpvp10@gmail.com>
2025-12-24 12:12:58 -0300

    adicionar a entrada do usuário de dois números

--- src/main.c
+++ src/main.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
 /* Calculadora feita em C!
- * feita por Matheus 
- *
- * */
+ * feita por Matheus
+*/
 
 int main(){
-
+  float num1;
+  float num2;
+  printf("Digite o 1º número: ");
+  scanf("%f", &num1);
+  printf("Digite o 2º número: ");
+  scanf("%f", &num2);
+  printf("OK!\n");
+  printf("1: %f, 2: %f\n", num1, num2);
   return 0;
 
 }
